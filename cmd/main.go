dd2106f762359aca5e0615fb25f93c7d904fb8f0
titkovnikita <nikita.titckow@ya.ru>
2024-11-03 12:51:27 +0300

    added get and getall methods

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,9 @@ import (
 	"github.com/NikitaTitkov/gRPC-Server-CRUD/pkg/users_v1"
 	"github.com/fatih/color"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -35,8 +37,42 @@ type Server struct {
 func (s *Server) Create(_ context.Context, req *users_v1.CreateIn) (*users_v1.CreateOut, error) {
 	users.mutex.Lock()
 	defer users.mutex.Unlock()
+	if _, ok := users.elements[req.User.GetID()]; ok {
+		return nil, status.Errorf(codes.AlreadyExists, "user with ID %d already exists", req.User.GetID())
+	}
 	users.elements[req.User.GetID()] = req.User
 	return &users_v1.CreateOut{ID: req.User.GetID()}, nil
+}
+
+// Get - returns the user associated with the specified ID
+func (s *Server) Get(_ context.Context, req *users_v1.GetIn) (*users_v1.GetOut, error) {
+	users.mutex.RLock()
+	defer users.mutex.RUnlock()
+	user := users.elements[req.ID]
+
+	if user == nil {
+		return nil, status.Errorf(codes.NotFound, "user with ID = %d not found", req.GetID())
+	}
+	return &users_v1.GetOut{User: user}, nil
+}
+
+// GetAll - method to get all users with limit and offset.
+func (s *Server) GetAll(_ context.Context, req *users_v1.GetAllIn) (*users_v1.GetAllOut, error) {
+	userSlice := make([]*users_v1.User, 0, len(users.elements))
+	position := int64(0)
+	users.mutex.RLock()
+	defer users.mutex.RUnlock()
+	for _, user := range users.elements {
+		if position < req.GetOffset() {
+			position++
+			continue
+		}
+		userSlice = append(userSlice, user)
+		if len(userSlice) == int(req.GetLimit()) {
+			break
+		}
+	}
+	return &users_v1.GetAllOut{Users: userSlice}, nil
 }
 
 func main() {
