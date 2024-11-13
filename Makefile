aa1c2ce0fcb44f790b0dd1e1b5550fa320b4116b
titkovnikita <nikita.titckow@ya.ru>
2024-11-13 19:31:52 +0300

    add handlers path

--- Makefile
+++ Makefile
@@ -22,3 +22,11 @@ generate-users-api:
 	--go-grpc_out=pkg/users_v1 --go-grpc_opt=paths=source_relative \
 	--plugin=protoc-gen-go-grpc=bin/protoc-gen-go-grpc \
 	api/users_v1/users.proto
+
+build:
+	GOOS=linux GOARCH=amd64 go build -o service_linux cmd/server/main.go
+
+docker-build-and-push:
+	docker buildx build --no-cache --platform linux/amd64 -t <REGESTRY>/my-server:v0.0.1 .
+	docker login -u <USERNAME> -p <PASSWORD> <REGESTRY>
+	docker push <REGESTRY>/my-server:v0.0.1
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,129 +1,33 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"net"
-	"sync"
 
+	"github.com/NikitaTitkov/gRPC-Server-CRUD/pkg/handlers"
 	"github.com/NikitaTitkov/gRPC-Server-CRUD/pkg/users_v1"
 	"github.com/fatih/color"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 const (
 	baseurl = "localhost:50051"
 )
 
-type syncMap struct {
-	elements map[int64]*users_v1.User
-	mutex    sync.RWMutex
-}
-
-var (
-	users = &syncMap{elements: make(map[int64]*users_v1.User)}
-)
-
-// Server represents the unimplemented gRPC server that handles user-related requests.
-type Server struct {
-	users_v1.UnimplementedUsersV1Server
-}
-
-// Create - creates a new user.
-func (s *Server) Create(_ context.Context, req *users_v1.CreateIn) (*users_v1.CreateOut, error) {
-	users.mutex.Lock()
-	defer users.mutex.Unlock()
-	if _, ok := users.elements[req.User.GetID()]; ok {
-		return nil, status.Errorf(codes.AlreadyExists, "user with ID %d already exists", req.User.GetID())
-	}
-	users.elements[req.User.GetID()] = req.User
-	return &users_v1.CreateOut{ID: req.User.GetID()}, nil
-}
-
-// Get - returns the user associated with the specified ID
-func (s *Server) Get(_ context.Context, req *users_v1.GetIn) (*users_v1.GetOut, error) {
-	users.mutex.RLock()
-	defer users.mutex.RUnlock()
-	user := users.elements[req.ID]
-
-	if user == nil {
-		return nil, status.Errorf(codes.NotFound, "user with ID = %d not found", req.GetID())
-	}
-	return &users_v1.GetOut{User: user}, nil
-}
-
-// GetAll - method to get all users with limit and offset.
-func (s *Server) GetAll(_ context.Context, req *users_v1.GetAllIn) (*users_v1.GetAllOut, error) {
-	userSlice := make([]*users_v1.User, 0, len(users.elements))
-	position := int64(0)
-	users.mutex.RLock()
-	defer users.mutex.RUnlock()
-	for _, user := range users.elements {
-		if position < req.GetOffset() {
-			position++
-			continue
-		}
-		userSlice = append(userSlice, user)
-		if len(userSlice) == int(req.GetLimit()) {
-			break
-		}
-	}
-	return &users_v1.GetAllOut{Users: userSlice}, nil
-}
-
-// Update - method that updates a user in the in-memory users database.
-func (s *Server) Update(_ context.Context, req *users_v1.UpdateIn) (*emptypb.Empty, error) {
-	users.mutex.Lock()
-	defer users.mutex.Unlock()
-	if _, ok := users.elements[req.GetID()]; !ok {
-		return nil, status.Errorf(codes.NotFound, "user with ID = %d not found", req.GetID())
-	}
-	if req.User.GetName() != nil {
-		users.elements[req.GetID()].Name = req.User.GetName().GetValue()
-	}
-	if req.User.GetAge() != nil {
-		users.elements[req.GetID()].Age = req.User.GetAge().GetValue()
-	}
-	if req.User.GetEmail() != nil {
-		users.elements[req.GetID()].Email = req.User.GetEmail().GetValue()
-	}
-	if req.User.Info.GetStreet() != nil {
-		users.elements[req.GetID()].Info.Street = req.User.Info.GetStreet().GetValue()
-	}
-	if req.User.Info.GetCity() != nil {
-		users.elements[req.GetID()].Info.City = req.User.Info.GetCity().GetValue()
-	}
-	return &emptypb.Empty{}, nil
-}
-
-// Delete - method that deletes a user from the in-memory users database.
-func (s *Server) Delete(_ context.Context, req *users_v1.DeleteIn) (*emptypb.Empty, error) {
-	users.mutex.Lock()
-	defer users.mutex.Unlock()
-	if _, ok := users.elements[req.GetID()]; !ok {
-		return nil, status.Errorf(codes.NotFound, "user with ID = %d not found", req.GetID())
-	}
-	delete(users.elements, req.GetID())
-	return &emptypb.Empty{}, nil
-}
-
 func main() {
 	l, err := net.Listen("tcp", baseurl)
 	if err != nil {
-		log.Fatal(color.RedString("Listen	 error: "), err)
+		logrus.Fatal(color.RedString("Listen error: "), err)
 
 	}
 	s := grpc.NewServer()
 	reflection.Register(s)
-	users_v1.RegisterUsersV1Server(s, &Server{})
+	users_v1.RegisterUsersV1Server(s, &handlers.Server{})
 	fmt.Println(color.GreenString("Server is running!"))
 
 	if err := s.Serve(l); err != nil {
-		log.Fatal(color.RedString("Serve error: "), err)
+		logrus.Fatal(color.RedString("Serve error: "), err)
 	}
 }
